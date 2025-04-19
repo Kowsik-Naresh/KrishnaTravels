4d927ff0a60112dbade5e931026cfef1732289c3
Pattapu Naresh <nareshpattapu9347@gmail.com>
2025-04-20 03:47:59 +0530

    first commit

---
+++ src/main/java/com/KT/KrishnaTravels/KrishnaTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.KT.KrishnaTravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishnaTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrishnaTravelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KT/KrishnaTravels/KrishnaTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KT.KrishnaTravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrishnaTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

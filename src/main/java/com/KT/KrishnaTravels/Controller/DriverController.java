156ae464d392dbded3c75c6f3c6786e3ee93ca73
kowsik <pkowsik2004@gmail.com>
2025-04-25 22:58:58 +0530

    created webconfig

--- src/main/java/com/KT/KrishnaTravels/Controller/DriverController.java
+++ src/main/java/com/KT/KrishnaTravels/Controller/DriverController.java
@@ -3,6 +3,7 @@ package com.KT.KrishnaTravels.Controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,7 +59,7 @@ public class DriverController {
 	 * @param DriverBean
 	 * @return KrishnaTravelsResponseBean with success or error message
 	 */
-    
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/createDriver")
     public KrishnaTravelsResponseBean createDriver(@RequestBody DriverBean driver) {
     	KrishnaTravelsResponseBean response=new KrishnaTravelsResponseBean();
